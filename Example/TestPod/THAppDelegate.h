b05ad902e9d7588c2e4ed90081f671d8a53d9b76
Tony <chenglei.wangll@gmail.com>
2017-03-02 10:27:27 +0800

    Initial commit

---
+++ Example/TestPod/THAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  THAppDelegate.h
+//  TestPod
+//
+//  Created by Tony on 03/02/2017.
+//  Copyright (c) 2017 Tony. All rights reserved.
+//
+
+@import UIKit;
+
+@interface THAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/TestPod/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestPod
+//
+//  Created by Tony on 03/02/2017.
+//  Copyright (c) 2017 Tony. All rights reserved.
+//
+
+@import UIKit;
+#import "THAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([THAppDelegate class]));
+    }
+}
